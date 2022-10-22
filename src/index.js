9f5cbc6dd7ae268f975401f4bc8424dba5f415e6
nunegabriel <nune.gabriel.gn@gmail.com>
2022-10-22 11:00:16 +0000

    API ADDED

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './style.css';
-import displayScore from './modules/scores';
+import postFunction from './modules/post';
+import getFunction from './modules/get';
 
-displayScore();
+const refresh = document.querySelector('.btn-1');
+
+const form = document.querySelector('form');
+
+refresh.addEventListener('click', getFunction);
+
+getFunction();
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  const user = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+  postFunction(user, score);
+  form.reset();
+};
